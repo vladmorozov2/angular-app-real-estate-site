f3032907116b2782646038b2d092b59a69f505c9
Morozov Vladyslav <vladmorozov32@outlook.com>
2024-01-10 03:08:59 +0200

    created angular app throught tutorial

---
+++ src/app/housing.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Housinglocation } from './housinglocation';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  url = 'http://localhost:3000/locations';
+
+  async getAllHousingLocations(): Promise<Housinglocation[]>{
+    const data = await fetch(this.url);
+    return await data.json() ?? []
+  }
+  
+  async getHousingLocationById(id: number):
+   Promise<Housinglocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
+  }
+
+  submitApplication(firstName: string,lastName: string,email: string){
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`)
+  }
+
+
+  constructor() { }
+}
